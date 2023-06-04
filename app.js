c7793dc1b6aa8586e7ce40f693cb04a10d658b4f
Elisabeth ROBERT AOUCHER <elisabeth.robert.aoucher@gmail.com>
2023-06-04 15:12:08 +0200

    rajout

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ let tabObjet=['table','chaise','lit','armoire','lampe','tapis','rideaux','four',
 
 const body=document.querySelector('body')
 const nouvelleDiv=document.createElement('div');
-const nouveauH2=document.createElement('h2');
-const contenuH2=document.createTextNode('Nouveau mot à trouver');
-nouveauH2.appendChild(contenuH2)
-nouvelleDiv.appendChild(nouveauH2);
+
 const inputSaisieParUtilisteur=document.getElementById("lettre")
 const divLettreInput=document.querySelector('.divLettreInput')
+
+// let compteur=10;
+let compteurGagner=0;
 
 const recupererBoutonVoir=document.getElementById('voir')
 
@@ -19,28 +19,79 @@ const recupererBoutonVoir=document.getElementById('voir')
 
 const categories=document.querySelectorAll(".categories");
 categories.forEach((bouton)=>{
-
+    
     bouton.addEventListener('click',choisirCategorie);
-
+    
   
 })
 
 function choisirCategorie(e){
+   
+//    compteur=10;
+    // compteur=10;
+    supprimerCreerDivCompteur()
+    effacerPerdu()
+    // let compteurFonction=quiInitialiseCompteur();
+  
+   
+ 
+    // alert(compteurFonction)
     
     faireApparaitreDivLettreInputActive();
+
     let quelCategories=e.target.dataset.categorie;
     let tableau=choisirTableau(quelCategories);
     let motsAleatoires=recupererMotAleatoire(tableau);
-    // alert(motsAleatoires)
+    alert(motsAleatoires)
    
     let combienDeMots=compterLesMots(motsAleatoires);
-
+    effacerH2()
+    creerH2();
     effacerElement();
     creationElement(combienDeMots);
 
+    let compteur=10;
+    // let compteurActuel=compteur;
+    // alert(compteur)
+  
+
+
     recupererBoutonVoir.addEventListener('click',function(e){
+        effacerPerdu();
         recupererMot(combienDeMots,motsAleatoires);
-        effacerInput();
+        effacerInput(); 
+        
+        compteur--
+      
+        // alert(compteur)
+       
+        // compteurActuel--;
+        // recupererIdCompteur
+
+        
+
+
+       
+       
+        // compteurActuel=quiComptabiliseCompteur(compteurActuel)
+        // alert(compteurActuel)
+        supprimerCreerDivCompteur()
+        creerDivCompteur(compteur)
+        // alert(compteurFonction)
+      
+
+        if(compteur===0){
+           compteur=10;
+            faireDisparaitreDivLettreInputActive();
+            effacerElement();
+            supprimerCreerDivCompteur()
+            effacerH2();
+            perduOuGagner('perdu');
+           
+         
+        }
+    
+        
        
     })
 
@@ -86,18 +137,44 @@ function creationElement(combienDeMots){
 
 function recupererMot(combienDeMots,motsAleatoires){
     const recupererSpanContenu=document.querySelectorAll('.spanContenu')
-   
     for(let i=0;i<combienDeMots;i++){
+        // alert(combienDeMots)
         if(inputSaisieParUtilisteur.value===motsAleatoires[i]){    
-           recupererSpanContenu[i].textContent=inputSaisieParUtilisteur.value
-        }     
+           recupererSpanContenu[i].textContent=inputSaisieParUtilisteur.value;
+           compteurGagner++;
+
+           if(compteurGagner===combienDeMots){
+                compteur=10;
+                faireDisparaitreDivLettreInputActive();
+                effacerElement();
+                supprimerCreerDivCompteur()
+                effacerH2();
+            perduOuGagner('gagner')
+           }
+         
+        }    
+        
     } 
+}
+function creerH2(){
+    const nouveauH2=document.createElement('h2');
+    const contenuH2=document.createTextNode('Nouveau mot à trouver');
+    nouveauH2.appendChild(contenuH2)
+    nouvelleDiv.appendChild(nouveauH2);
+    nouveauH2.id='nouveauH2';
+}
+
+function effacerH2(){
+    const nouveauH2=document.getElementById('nouveauH2')
+    if(nouveauH2!==null){
+        nouveauH2.remove();
+    }
 }
 
 function creerElement(){
     const nouveauDivSpan=document.createElement('span');
     // const nouveauInput=document.createElement('input');
-   
+    
     const nouveauContenu=document.createTextNode('_ ');
     // nouveauDivSpan.appendChild(nouveauInput)
     nouveauDivSpan.appendChild(nouveauContenu);
@@ -131,9 +208,16 @@ function effacerInput(){
 
 function faireApparaitreDivLettreInputActive(){
     divLettreInput.classList.remove('divLettreInput');
-    divLettreInput.classList.add('divLettreInputActive')
+    divLettreInput.classList.add('divLettreInputActive');
     
 } 
+
+function faireDisparaitreDivLettreInputActive(){
+    const divLettreInputActive=document.getElementsByClassName('divLettreInputActive');
+    divLettreInput.classList.remove('divLettreInputActive');
+    divLettreInput.classList.add('divLettreInput')
+    
+}
 
 function faireApparaitreClasse(couleurs){
     couleurs.classList.add('choisirBoutonsActive')
@@ -144,6 +228,70 @@ function faireDisparaitreAutresClasses(){
         element.classList.remove('choisirBoutonsActive')
     })
 }
+
+function creerDivCompteur(compteur){
+    alert(compteur)
+    const creationDivCompteur=document.createElement("div");
+    const creationContenuCompteur=document.createTextNode(`Il vous reste ${compteur} tentatives`);
+    creationDivCompteur.appendChild(creationContenuCompteur);
+    body.appendChild(creationDivCompteur);
+    creationDivCompteur.id='divCompteur';
+
+
+}
+
+function supprimerCreerDivCompteur(){
+    let recupererIdCompteur=document.getElementById('divCompteur');
+    if(recupererIdCompteur!=null){
+        recupererIdCompteur.remove();
+    }
+       
+}
+
+function perduOuGagner(perduOuGagner){
+    let nouvelleDivPerdu=document.createElement('div');
+    let nouveauPargraphePerdu=document.createElement('p');
+    nouvelleDivPerdu.appendChild(nouveauPargraphePerdu);
+    nouveauPargraphePerdu.textContent=`Vous avez ${perduOuGagner} .Cliquez sur un bouton pour recommencer`;
+    body.appendChild(nouvelleDivPerdu);
+    nouveauPargraphePerdu.id='nouveauParagraphePerduOuGagner'
+}
+
+function effacerPerdu(){
+    const nouveauParagraphePerdu=document.getElementById('nouveauParagraphePerduOuGagner');
+    if(nouveauParagraphePerdu!==null){
+        nouveauParagraphePerdu.remove();
+    }
+}
+
+
+
+// function quiComptabiliseCompteur(compteur){
+    
+//     compteur--;
+//     if(compteur===0){
+//         compteur=10;
+//          faireDisparaitreDivLettreInputActive();
+//          effacerElement();
+//          supprimerCreerDivCompteur()
+//          effacerH2();
+//          perdu();
+        
+      
+//      }
+
+//      return compteur;
+    
+// }
+
+// function quiInitialiseCompteur(){
+//     let compteur=10;
+//     return compteur
+// }
+
+
+
+
 
 
 
